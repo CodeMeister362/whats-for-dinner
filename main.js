d18fd983b2830a40a2e5a16f65ec81992fc20d80
Nathan Chamberlain <chamberlainuxdesign@gmail.com>
2023-02-17 13:59:02 -0800

    Adds completed UI - radio btns, lets cook btn, svg image

---
+++ main.js
@@ -0,0 +1,12 @@
+//querySelector
+var thisSide = document.querySelector('#side');
+var button = document.querySelector('.lets-cook-btn');
+var thisMainDish = document.querySelector('#Main-Dish');
+var thisDessert = document.querySelector('#Dessert');
+var thisEntireMeal = document.querySelector('#Entire-Meal');
+//event listener
+button.addEventListener('click', displayFood);
+//event handler
+function displayFood() {
+  if()
+}
