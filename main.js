3fa81b606da350e284f68af3e8e622d711f1f67e
Nathan Chamberlain <chamberlainuxdesign@gmail.com>
2023-02-18 15:49:15 -0800

    Adds half functioning side radio btn: randomly selections side from side array - next step display choice.

--- main.js
+++ main.js
@@ -1,32 +1,26 @@
 //querySeletor 
-var radioBtn = document.querySelectorAll('input[name="btn"]');
+var radioBtn = document.querySelector('#side');
 var letsCookbtn = document.querySelector('.lets-cook-btn');
 var outPut = document.querySelector('#outPut');
 //event listener 
-letsCookbtn.addEventListener('click',displayRecipe);
 radioBtn.addEventListener('click', getRandomRecipe);
+letsCookbtn.addEventListener('click',displayRecipe)
 //event handler
-function displayRecipe(dish) {
-  
-}
-function getRandomRecipe() {
-  var recipeArray;
-  if (radioBtn.checked) {
-    recipeArray = radioBtn.value;
-  }
-}
-
-output.innerText = recipeArray ? `You should make: ${}`
-
-
 function getRandomIndex(array) {
-  return Math.floor(Math.random() * array.length);
-}
-function randomize() {
- var randomQuote = quotes[randomQuoteIndex];
- document.querySelector('.poster-quote').textContent = randomQuote;
+  return Math.floor(Math.random()*array.length)
 }
 
-var dessertRecipe = ['Apple Pie','Lemon Meringue Pie','Black Forest Cake','Banana Bread','Peach Cobbler','Cheesecake','Funfetti Cake','Baklava','Flan','Macarons','Macaroons','Chocolate Cupcakes','Pavlova','Pumpkin Pie','Key Lime Pie','Tart Tatin','Croissants','Eclairs'];
+function getRandomRecipe() {
+  var randomRecipeIndex = getRandomIndex(sideRecipes);
+  var randomRecipe = sideRecipes[randomRecipeIndex];
+  console.log(randomRecipe)
+}
+
+function displayRecipe() {
+
+}
+
+
+var dessertRecipes = ['Apple Pie','Lemon Meringue Pie','Black Forest Cake','Banana Bread','Peach Cobbler','Cheesecake','Funfetti Cake','Baklava','Flan','Macarons','Macaroons','Chocolate Cupcakes','Pavlova','Pumpkin Pie','Key Lime Pie','Tart Tatin','Croissants','Eclairs'];
 var mainRecipes = ['Spaghetti and Meatballs','Pineapple Chicken','Shakshuka','Thai Yellow Curry','Bibimbap','Chicken Parmesean','Butternut Squash Soup','BBQ Chicken Burgers','Ramen','Empanadas','Chicken Fried Rice','Sheet Pan Fajitas','Margarita Pizza'];
 var sideRecipes = ['Miso Glazed Carrots','Coleslaw','Garden Salad','Crispy Potatoes','Sweet Potato Tots','Coconut Rice','Caeser Salad','Shrimp Summer Rolls','Garlic Butter Mushrooms','Hush Puppies'];
