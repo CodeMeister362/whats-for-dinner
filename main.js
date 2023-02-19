91ff41d1c16babf670b9417112d22e89a20cd0cf
Nathan Chamberlain <chamberlainuxdesign@gmail.com>
2023-02-19 14:02:26 -0800

    Adds refactored code

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-//querySeletor 
 var side = document.querySelector('#side'),
     main = document.querySelector('#main-dish'),
     dessert = document.querySelector('#dessert'),
@@ -8,36 +7,35 @@ var side = document.querySelector('#side'),
     hidePotImg = document.querySelector('#pot-img'),
     displayTxt = document.querySelector('.you-should-make');
  
-//event listener
-letsCookBtn.addEventListener('click', hidePot, getRandomRecipe);
-//event handler
+  letsCookBtn.addEventListener('click', hidePot, getRandomRecipe);
+
 function getRandomIndex(array) {
   return Math.floor(Math.random()*array.length)
 }
 
 function getRandomRecipe() {
+  var randomRecipeIndex1 = getRandomIndex(mainRecipes),
+      randomRecipeIndex2 = getRandomIndex(sideRecipes),
+      randomRecipeIndex3 = getRandomIndex(dessertRecipes),
+      randomRecipe1 = mainRecipes[randomRecipeIndex1],
+      randomRecipe2 = sideRecipes[randomRecipeIndex2],
+      randomRecipe3 = dessertRecipes[randomRecipeIndex3]
+
   if (side.checked) {
-    var randomRecipeIndex = getRandomIndex(sideRecipes),
-        randomRecipe = sideRecipes[randomRecipeIndex];
+      document.querySelector('#array-output').textContent = (randomRecipe2)
   }
+
   if (main.checked) {
-    var randomRecipeIndex = getRandomIndex(mainRecipes),
-        randomRecipe = mainRecipes[randomRecipeIndex];
+    document.querySelector('#array-output').textContent = (randomRecipe1)
   }
+
   if (dessert.checked) {
-    var randomRecipeIndex = getRandomIndex(dessertRecipes),
-        randomRecipe = dessertRecipes[randomRecipeIndex];
+    document.querySelector('#array-output').textContent = (randomRecipe3)
   }
+
   if (entireMeal.checked) {
-    var randomRecipeIndex1 = getRandomIndex(mainRecipes),
-        randomRecipe1 = mainRecipes[randomRecipeIndex1],
-        randomRecipeIndex2 = getRandomIndex(sideRecipes),
-        randomRecipe2 = sideRecipes[randomRecipeIndex2],
-        randomRecipeIndex3 = getRandomIndex(dessertRecipes),
-        randomRecipe3 = dessertRecipes[randomRecipeIndex3];
-    return document.querySelector('#array-output').textContent = `${randomRecipe1} with a side of ${randomRecipe2} and ${randomRecipe3} for dessert!`;
+     document.querySelector('#array-output').textContent = `${randomRecipe1} with a side of ${randomRecipe2} and ${randomRecipe3} for dessert!`;
   }
-  document.querySelector('#array-output').textContent = (randomRecipe)
 }
 
 function hidePot() {
@@ -48,7 +46,6 @@ function hidePot() {
 
 
 
-var  dessertRecipes = ['Apple Pie','Lemon Meringue Pie','Black Forest Cake','Banana Bread','Peach Cobbler','Cheesecake','Funfetti Cake','Baklava','Flan','Macarons','Macaroons','Chocolate Cupcakes','Pavlova','Pumpkin Pie','Key Lime Pie','Tart Tatin','Croissants','Eclairs'],
-     mainRecipes = ['Spaghetti and Meatballs','Pineapple Chicken','Shakshuka','Thai Yellow Curry','Bibimbap','Chicken Parmesean','Butternut Squash Soup','BBQ Chicken Burgers','Ramen','Empanadas','Chicken Fried Rice','Sheet Pan Fajitas','Margarita Pizza'],
-     sideRecipes = ['Miso Glazed Carrots','Coleslaw','Garden Salad','Crispy Potatoes','Sweet Potato Tots','Coconut Rice','Caeser Salad','Shrimp Summer Rolls','Garlic Butter Mushrooms','Hush Puppies'],
-     entireMealRecipes = [];
+var dessertRecipes = ['Apple Pie','Lemon Meringue Pie','Black Forest Cake','Banana Bread','Peach Cobbler','Cheesecake','Funfetti Cake','Baklava','Flan','Macarons','Macaroons','Chocolate Cupcakes','Pavlova','Pumpkin Pie','Key Lime Pie','Tart Tatin','Croissants','Eclairs'],
+    mainRecipes = ['Spaghetti and Meatballs','Pineapple Chicken','Shakshuka','Thai Yellow Curry','Bibimbap','Chicken Parmesean','Butternut Squash Soup','BBQ Chicken Burgers','Ramen','Empanadas','Chicken Fried Rice','Sheet Pan Fajitas','Margarita Pizza'],
+    sideRecipes = ['Miso Glazed Carrots','Coleslaw','Garden Salad','Crispy Potatoes','Sweet Potato Tots','Coconut Rice','Caeser Salad','Shrimp Summer Rolls','Garlic Butter Mushrooms','Hush Puppies'];
