ed38b400e1642173463f8d1f0c2d4b69f6d16e5e
Nathan Chamberlain <chamberlainuxdesign@gmail.com>
2023-02-19 11:31:21 -0800

    Adds completed iteration 1: side, main dish, dessert display on btn click.

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
 //querySeletor 
 var side = document.querySelector('#side'),
     main = document.querySelector('#main-dish'),
-    letsCookbtn = document.querySelector('.lets-cook-btn'),
+    dessert = document.querySelector('#dessert'),
+    entireMeal = document.querySelector('#entire-meal'),
+    letsCookBtn = document.querySelector('.lets-cook-btn'),
     outPut = document.querySelector('#outPut'),
     hidePotImg = document.querySelector('#pot-img'),
-    displaytxt = document.querySelector('.you-should-make');
+    displayTxt = document.querySelector('.you-should-make');
  
 //event listener
-letsCookbtn.addEventListener('click', hidePot, getRandomRecipe);
+letsCookBtn.addEventListener('click', hidePot, getRandomRecipe);
 //event handler
 function getRandomIndex(array) {
   return Math.floor(Math.random()*array.length)
@@ -22,17 +24,26 @@ function getRandomRecipe() {
     var randomRecipeIndex = getRandomIndex(mainRecipes),
         randomRecipe = mainRecipes[randomRecipeIndex];
   }
+  if (dessert.checked) {
+    var randomRecipeIndex = getRandomIndex(dessertRecipes),
+        randomRecipe = dessertRecipes[randomRecipeIndex];
+  }
+  if (entireMeal.checked) {
+    var randomRecipeIndex = getRandomIndex(sideRecipes),
+        randomRecipe = sideRecipes[randomRecipeIndex];
+  }
   document.querySelector('#array-output').textContent = randomRecipe
 }
 
 function hidePot() {
   hidePotImg.classList.add('hide');
-  displaytxt.classList.remove('hide');
+  displayTxt.classList.remove('hide');
   getRandomRecipe();
 }
 
 
 
-var dessertRecipes = ['Apple Pie','Lemon Meringue Pie','Black Forest Cake','Banana Bread','Peach Cobbler','Cheesecake','Funfetti Cake','Baklava','Flan','Macarons','Macaroons','Chocolate Cupcakes','Pavlova','Pumpkin Pie','Key Lime Pie','Tart Tatin','Croissants','Eclairs'];
-var mainRecipes = ['Spaghetti and Meatballs','Pineapple Chicken','Shakshuka','Thai Yellow Curry','Bibimbap','Chicken Parmesean','Butternut Squash Soup','BBQ Chicken Burgers','Ramen','Empanadas','Chicken Fried Rice','Sheet Pan Fajitas','Margarita Pizza'];
-var sideRecipes = ['Miso Glazed Carrots','Coleslaw','Garden Salad','Crispy Potatoes','Sweet Potato Tots','Coconut Rice','Caeser Salad','Shrimp Summer Rolls','Garlic Butter Mushrooms','Hush Puppies'];
+var dessertRecipes = ['Apple Pie','Lemon Meringue Pie','Black Forest Cake','Banana Bread','Peach Cobbler','Cheesecake','Funfetti Cake','Baklava','Flan','Macarons','Macaroons','Chocolate Cupcakes','Pavlova','Pumpkin Pie','Key Lime Pie','Tart Tatin','Croissants','Eclairs'],
+     mainRecipes = ['Spaghetti and Meatballs','Pineapple Chicken','Shakshuka','Thai Yellow Curry','Bibimbap','Chicken Parmesean','Butternut Squash Soup','BBQ Chicken Burgers','Ramen','Empanadas','Chicken Fried Rice','Sheet Pan Fajitas','Margarita Pizza'],
+     sideRecipes = ['Miso Glazed Carrots','Coleslaw','Garden Salad','Crispy Potatoes','Sweet Potato Tots','Coconut Rice','Caeser Salad','Shrimp Summer Rolls','Garlic Butter Mushrooms','Hush Puppies'],
+     entireMealRecipes = [];
